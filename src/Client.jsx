416d678845d1fc87369aac51662cc01f697a5e83
Imen <daoud_imen@yahoo.fr>
2023-07-05 13:49:56 +0100

    first commit

---
+++ src/Client.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react"
+class Client extends Component{
+    render()
+    {
+        return(
+            <div>Bonjour Madame</div>
+        )
+    }
+}
+export default Client
